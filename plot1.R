e593b62eac7849b8652352438418f971ffe3e8d8
Santiago <sn377@cornell.edu>
2015-03-08 04:23:40 +0100

    updated all files

--- plot1.R
+++ plot1.R
@@ -9,6 +9,7 @@ Power$Date <- as.Date(Power$Date, format="%d/%m/%Y")
 
 NewPower <- subset(Power, Date== "2007-02-01" |  Date== "2007-02-02")
 
-png("Plot1.png")
+png("plot1.png")
+par(bg=NA) 
 hist(NewPower$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red")
 dev.off()
